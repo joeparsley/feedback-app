4634cfd1341f8db119b9a44cc771503904b7eb71
Joe Parsley <parsley@Joes-MacBook-Pro.local>
2022-01-03 21:22:47 -0800

    Sections 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+function App () {
+    const title = 'Blog Posts'
+    const body = 'This is the body text'
+    const comments =[
+        {id: 1, text:'Comment One'},
+        {id: 2, text:'Comment Two'},
+        {id: 3, text:'Comment Three'}
+    ]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const loading = false
+    const showComments = true
+
+    const commentBlock = (
+        <div className="comments">
+            <h3>Comments ({comments.length})</h3>
+            <ul>
+                {comments.map((comment, index) => (
+                    <li key={index}>{comment.text}</li>
+                ))}
+            </ul>
+        </div> 
+    )
+
+    if (loading) return <h1>Loading...</h1>
+
+    return(
+        <div className="container">
+            <h1>{title}</h1>
+            <p>{body}</p>
+            {showComments && commentBlock}
+        </div>
+    )
 }
 
-export default App;
+export default App
