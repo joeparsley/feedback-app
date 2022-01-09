0202a3c55323c48a248165e3074b5d5cfeeca4db
Joe Parsley <parsley@Joes-MacBook-Pro.local>
2022-01-09 11:34:13 -0800

    Complete Section 3: Components, Props and State

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackData from './data/FeedbackData'
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+
 function App () {
-    const title = 'Blog Posts'
-    const body = 'This is the body text'
-    const comments =[
-        {id: 1, text:'Comment One'},
-        {id: 2, text:'Comment Two'},
-        {id: 3, text:'Comment Three'}
-    ]
 
-    const loading = false
-    const showComments = true
+    const [feedback, setFeedback] = useState(FeedbackData)
 
-    const commentBlock = (
-        <div className="comments">
-            <h3>Comments ({comments.length})</h3>
-            <ul>
-                {comments.map((comment, index) => (
-                    <li key={index}>{comment.text}</li>
-                ))}
-            </ul>
-        </div> 
-    )
-
-    if (loading) return <h1>Loading...</h1>
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are you Sure you want to delete?')) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
 
     return(
-        <div className="container">
-            <h1>{title}</h1>
-            <p>{body}</p>
-            {showComments && commentBlock}
-        </div>
+        <>
+            <Header />
+            <div className='container'>
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList 
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}
+                />
+            </div>
+        </>
     )
 }
 
