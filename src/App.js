ff93a6bf9d6ce1d02116bb738d03070e3ca2bbd6
Joe Parsley <joewparsley@gmail.com>
2022-01-16 10:53:16 -0800

    Section 5: React router 5 and 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { v4 as uuidv4 } from 'uuid'
 import { useState } from 'react'
-import FeedbackData from './data/FeedbackData'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './pages/AboutPage'
+import AboutIconLink from './components/AboutIconLink'
+import FeedbackData from './data/FeedbackData'
 
 function App () {
 
@@ -22,17 +25,29 @@ function App () {
     }
 
     return(
-        <>
+        <Router>
             <Header />
             <div className='container'>
-                <FeedbackForm handleAdd={addFeedback }/>
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList 
-                    feedback={feedback}
-                    handleDelete={deleteFeedback}
-                />
+                <Routes>
+                    <Route exact path='/' element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback }/>
+                            <FeedbackStats feedback={feedback} />
+                            <FeedbackList 
+                                feedback={feedback}
+                                handleDelete={deleteFeedback}
+                            /> 
+                        </>
+                    }>  
+                    </Route>
+
+
+                    <Route exact path='/about' element={<AboutPage />} />
+                </Routes>
+                
+                <AboutIconLink />
             </div>
-        </>
+        </Router>
     )
 }
 
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import Card from "../components/shared/Card"
+import { Link } from 'react-router-dom'
+
+function AboutPage() {
+    return (
+        <Card>
+            <div className="about">
+                <h1>About this Project</h1>
+                <p>This is a React app for leaving feedback on a product or service</p>
+                <p>Version: 1.0.0</p>
+                <p>
+                    <Link to="/">Back to Home Page</Link>
+                </p>
+            </div>
+        </Card>
+    )
+}
+
+export default AboutPage
